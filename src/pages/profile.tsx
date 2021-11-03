dec2cf9e414855af3ab76ae64b73117ed470915e
Michael Romay <amerror@gmail.com>
2021-11-02 17:32:03 -0700

    reverted back to old profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,96 +1,32 @@
-import { useEffect, useState } from 'react'
-import Avatar from '../components/Avatar/Avatar'
-// import { GetServerSideProps } from 'next'
-// import Link from 'next/link'
-// import Router from 'next/router'
+import { useEffect } from 'react'
+import { GetServerSideProps } from 'next'
+import Link from 'next/link'
+import Router from 'next/router'
 import { supabase } from '~/lib/supabase'
-// import { useAuth, ProtectedRoute } from '~/lib/auth'
+import { useAuth, ProtectedRoute } from '~/lib/auth'
 import Layout from '~/components/Layout'
-// import { SpinnerFullPage } from '~/components/Spinner'
-// import { ROUTE_AUTH } from '~/config'
-// import { NextAppPageServerSideProps } from '~/types/app'
+import { SpinnerFullPage } from '~/components/Spinner'
+import { ROUTE_AUTH } from '~/config'
+import { NextAppPageServerSideProps } from '~/types/app'
 
-// const ProfilePage = (props) => {
-const ProfilePage = ({ session }) => {
-  // const { user, userLoading, signOut, loggedIn } = useAuth()
-  const [loading, setLoading] = useState(true)
-  const [username, setUsername] = useState(null)
-  const [website, setWebsite] = useState(null)
-  const [avatar_url, setAvatarUrl] = useState(null)
-
-  // useEffect(() => {
-  //   if (!userLoading && !loggedIn) {
-  //     Router.push(ROUTE_AUTH)
-  //   }
-  // }, [userLoading, loggedIn])
+const ProfilePage = (props) => {
+  const { user, userLoading, signOut, loggedIn } = useAuth()
 
   useEffect(() => {
-    getProfile()
-  }, [session])
-
-  async function getProfile() {
-    try {
-      setLoading(true)
-      const user = supabase.auth.user()
-
-      let { data, error, status } = await supabase
-        .from('profiles')
-        .select(`username, website, avatar_url`)
-        .eq('id', user.id)
-        .single()
-
-      if (error && status !== 406) {
-        throw error
-      }
-
-      if (data) {
-        setUsername(data.username)
-        setWebsite(data.website)
-        setAvatarUrl(data.avatar_url)
-      }
-    } catch (error) {
-      alert(error.message)
-    } finally {
-      setLoading(false)
+    if (!userLoading && !loggedIn) {
+      Router.push(ROUTE_AUTH)
     }
+  }, [userLoading, loggedIn])
+
+  if (userLoading) {
+    return <SpinnerFullPage />
   }
-
-  async function updateProfile({ username, website, avatar_url }) {
-    try {
-      setLoading(true)
-      const user = supabase.auth.user()
-
-      const updates = {
-        id: user.id,
-        username,
-        website,
-        avatar_url,
-        updated_at: new Date(),
-      }
-
-      let { error } = await supabase.from('profiles').upsert(updates, {
-        returning: 'minimal', // Don't return the value after inserting
-      })
-
-      if (error) {
-        throw error
-      }
-    } catch (error) {
-      alert(error.message)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  // if (userLoading) {
-  //   return <SpinnerFullPage />
-  // }
 
   return (
     <Layout useBackdrop={false}>
-      {/* <div className="h-screen flex flex-col justify-center items-center relative">
+      <div className="h-screen flex flex-col justify-center items-center relative">
         <h2 className="text-3xl my-4">
-          Howdie, {user && user.email ? user.email : 'Explorer'}!
+          Oh, hello, {user && user.email ? user.email : 'Explorer'}!
         </h2>
         {!user && (
           <small>
@@ -108,64 +44,37 @@ const ProfilePage = ({ session }) => {
             </button>
           </div>
         )}
-      </div> */}
-
-      <div className="form-widget">
-        <div>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="text" value={session.user.email} disabled />
-        </div>
-        <div>
-          <label htmlFor="username">Name</label>
-          <input
-            id="username"
-            type="text"
-            value={username || ''}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="website">Website</label>
-          <input
-            id="website"
-            type="website"
-            value={website || ''}
-            onChange={(e) => setWebsite(e.target.value)}
-          />
-        </div>
-        <Avatar
-          url={avatar_url}
-          size={150}
-          onUpload={(url) => {
-            setAvatarUrl(url)
-            updateProfile({ username, website, avatar_url: url })
-          }}
-        />
-
-        <div>
-          <button
-            className="button block primary"
-            onClick={() => updateProfile({ username, website, avatar_url })}
-            disabled={loading}
-          >
-            {loading ? 'Loading ...' : 'Update'}
-          </button>
-        </div>
-
-        <div>
-          <button
-            className="button block"
-            onClick={() => supabase.auth.signOut()}
-          >
-            Sign Out
-          </button>
-        </div>
       </div>
     </Layout>
   )
 }
 
 export default ProfilePage
+
+// Fetch user data server-side to eliminate a flash of unauthenticated content.
+// We're identifying the logged-in user through supabase cookies and either redirecting to  `/` if the user is not found, or sending the `user` and `loggedIn` props which can be available to the above component through `props`.
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+}): Promise<NextAppPageServerSideProps> => {
+  const { user } = await supabase.auth.api.getUserByCookie(req)
+  // We can do a re-direction from the server
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+  // or, alternatively, can send the same values that client-side context populates to check on the client and redirect
+  // The following lines won't be used as we're redirecting above
+  return {
+    props: {
+      user,
+      loggedIn: !!user,
+    },
+  }
+}
 
 // As there could be many pages that'll be required to be rendered only for the logged-in users, and the above logic
 // for indetifying authenticity could become repetitive, there's a wrapper component `ProtectedRoute` already available
