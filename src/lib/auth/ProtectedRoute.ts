9ab49254b56c1898b599521762c69885a07887dc
Michael Romay <amerror@gmail.com>
2022-05-30 14:32:59 -0700

    another one

--- src/lib/auth/ProtectedRoute.ts
+++ src/lib/auth/ProtectedRoute.ts
@@ -6,6 +6,7 @@ export type ProtectedRouteProps = {
   props: {
     user: User;
     loggedIn: boolean;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     [key: string]: any;
   };
 };
