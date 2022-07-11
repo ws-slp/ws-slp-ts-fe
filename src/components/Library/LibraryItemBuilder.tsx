cc6eb1408510ee07122853b733caf35658d41928
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-07-11 13:41:21 -0700

    WIP: abstracting library items to builder component

---
+++ src/components/Library/LibraryItemBuilder.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Modulars} from '~/pages/library';
+
+const LibraryItemBuilder: React.FunctionComponent<Modulars> = props => {
+  return (
+    <div key={props.modular_id}>
+      <h3>{props.name}</h3>
+      {props.quantity ? <p>In Stock</p> : <p>Out of Stock</p>}
+      {props.image_url ? (
+        <img src={props.image_url} alt={props.name} />
+      ) : (
+        <p>No image available.</p>
+      )}
+    </div>
+  );
+};
+
+export default LibraryItemBuilder;
--- src/pages/library.tsx
+++ src/pages/library.tsx
@@ -1,30 +1,32 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
 import Layout from '../components/Layout';
 import {useState, useEffect} from 'react';
 import {supabase} from '~/lib/supabase';
+import LibraryItemBuilder from '../components/Library/LibraryItemBuilder';
 
-interface Library {
-  modulars: {
-    modular_id: number;
-    created_at: Date;
-    updated_at: Date;
-    name: string;
-    quantity: number;
-    image_url: string | null;
-    manufacturer: string | null;
-    model: string | null;
-    weight: number | null;
-    description: string | null;
-    accessories: string | null;
-  };
+export interface Modulars {
+  modular_id: number;
+  created_at: Date;
+  updated_at: Date;
+  name: string;
+  quantity: number;
+  image_url: string | null;
+  manufacturer: string | null;
+  model: string | null;
+  weight: number | null;
+  description: string | null;
+  accessories: string | null;
 }
 
 const Library: React.FunctionComponent = () => {
-  const [modulars, setModulars] = useState<Library['modulars'][]>([]);
+  const [modulars, setModulars] = useState<Modulars[]>([]);
 
   useEffect(() => {
     const fetchModulars = async () => {
       const {error, data} = await supabase
-        .from<Library['modulars']>('modulars')
+        .from<Modulars>('modulars')
         .select('*');
       if (!error) {
         setModulars(data);
@@ -37,26 +39,10 @@ const Library: React.FunctionComponent = () => {
     <>
       <Layout>
         <h2>welcome to the library</h2>
-        {modulars.map(item => (
-          <div key={item.modular_id}>
-            <h3>{item.name}</h3>
-            {item.quantity ? <p>In Stock</p> : <p>Out of Stock</p>}
-            {item.image_url ? (
-              <img src={item.image_url} alt={item.name} />
-            ) : (
-              <p>No image available.</p>
-            )}
-            <ul>
-              Item Details
-              <li>Manufacturer: {item.manufacturer}</li>
-              <li>Model: {item.model}</li>
-              <li>Weight: {item.weight}</li>
-              <li>Accessories: {item.accessories}</li>
-            </ul>
-            Description:
-            <p>{item.description}</p>
-          </div>
-        ))}
+        {modulars.map(item => {
+          console.log(item);
+          <LibraryItemBuilder key={item.modular_id} props={item} />;
+        })}
       </Layout>
     </>
   );
