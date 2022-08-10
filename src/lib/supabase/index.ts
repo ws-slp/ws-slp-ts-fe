0f3663532b8418180369996ca13c09db9cd93efa
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-08-09 18:51:17 -0700

    update supabase index with supabase stub imports

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,1 +1,23 @@
 export * from './supabase';
+
+import {
+  getAllLibraryItems,
+  searchLibraryItemsByName,
+  searchLibraryItemByTags,
+  searchLibraryItemsByAvailability,
+  searchLibraryItemsByCategory,
+  searchAllLibraryItems,
+} from './library/library';
+
+const core = {
+  library: {
+    getAllLibraryItems,
+    searchLibraryItemsByName,
+    searchLibraryItemByTags,
+    searchLibraryItemsByAvailability,
+    searchLibraryItemsByCategory,
+    searchAllLibraryItems,
+  },
+};
+
+export default core;
