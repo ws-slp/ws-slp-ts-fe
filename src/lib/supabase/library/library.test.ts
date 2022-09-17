0c6ee33a04759e472ab3099d28ea91f0b12caf94
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-09-17 16:57:50 -0700

    adding async and await to test

--- src/lib/supabase/library/library.test.ts
+++ src/lib/supabase/library/library.test.ts
@@ -3,8 +3,8 @@ import core from '../index';
 const {getAllLibraryItems} = core.library;
 //TODO: actually write test for each cloud function.
 describe('call supabase stub functions', () => {
-  test('call function getAllLibraryItems', () => {
-    getAllLibraryItems();
+  test('call function getAllLibraryItems', async () => {
+    await getAllLibraryItems();
     expect(getAllLibraryItems).toHaveBeenCalled();
   });
 });
